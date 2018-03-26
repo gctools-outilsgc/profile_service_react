9aebb872ddb003b2962222b8dec6fe38ced046f0
Scott Buchanan <scott.buchanan@nrc-cnrc.gc.ca>
2018-03-26 16:42:04 -0300

    added i18n-edit components to page, made no UI changes.

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Segment, Dimmer, Loader, Item, Icon, Button, List }
   from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import LocalizedComponent
   from '@gctools-components/react-i18n-translation-webpack';
+import ReactI18nEdit from '@gctools-components/react-i18n-edit';
 
 const style = {
   imageExample: {
@@ -12,72 +13,173 @@ const style = {
   },
 };
 
-const ProfileInfo = (props) => {
-  const {
-    loading,
-    error,
-    profile,
-  } = props;
-  if (error) return `Error!: ${error}`;
-  return (
-    <Segment>
-      <Dimmer active={loading}>
-        <Loader content={__('Loading')} />
-      </Dimmer>
-      <Item.Group>
-        <Item>
-          <Item.Image style={style.imageExample} size="tiny" />
-          <Item.Content>
-            <Button floated="right" basic size="small">
-              <Icon size="tiny" name="edit" /> {__('Edit')}
-            </Button>
-            <Item.Header> {profile.name} </Item.Header>
-            <Item.Meta> {profile.titleEn} / {profile.titleFR} </Item.Meta>
-            <Item.Meta> {profile.org.nameEn} </Item.Meta>
-            <Item.Description style={{ marginTop: '20px' }}>
-              <List horizontal>
-                <List.Item>
-                  <List.Icon size="large" name="phone" />
-                  <List.Content>
-                    <List.Header> {__('Work')} </List.Header>
-                    <List.Description>
-                      {profile.officePhone}
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Icon size="large" name="mobile" />
-                  <List.Content>
-                    <List.Header> {__('Mobile')} </List.Header>
-                    <List.Description>
-                      {profile.mobilePhone}
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Icon size="large" name="point" />
-                  <List.Content>
-                    <List.Header> {__('Address')} </List.Header>
-                    <List.Description>
-                      {profile.address.streetAddress}
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Icon size="large" name="mail" />
-                  <List.Content>
-                    <List.Header>{__('Email')} </List.Header>
-                    <List.Description> {profile.email} </List.Description>
-                  </List.Content>
-                </List.Item>
-              </List>
-            </Item.Description>
-          </Item.Content>
-        </Item>
-      </Item.Group>
-    </Segment>
-  );
-};
+class ProfileInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editMode: false,
+      ready: false,
+      profile: {
+        org: {},
+        address: {},
+      },
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile &&
+        nextProps.profile !== this.props.profile) {
+      this.setState({
+        profile: nextProps.profile, ready: true,
+      });
+    }
+  }
+
+  render() {
+    const {
+      loading,
+      error,
+    } = this.props;
+    if (error) return `Error!: ${error}`;
+    if (this.state.ready === false) return false;
+    return (
+      <Segment>
+        <Dimmer active={loading}>
+          <Loader content={__('Loading')} />
+        </Dimmer>
+        <Item.Group>
+          <Item>
+            <Item.Image style={style.imageExample} size="tiny" />
+            <Item.Content>
+              <Button
+                floated="right"
+                size="small"
+                basic
+                onClick={() => this.setState({ editMode: true })}
+              >
+                <Icon size="tiny" name="edit" /> {__('Edit')}
+              </Button>
+              <Item.Header>
+                <ReactI18nEdit
+                  edit={this.state.editMode}
+                  values={[{
+                    value: this.state.profile.name,
+                    placeholder: 'name',
+                  }]}
+                  showLabel={false}
+                />
+              </Item.Header>
+              <Item.Meta>
+                <ReactI18nEdit
+                  edit={this.state.editMode}
+                  lang={localizer.lang}
+                  values={[
+                    {
+                      lang: 'en_CA',
+                      value: this.state.profile.titleEn,
+                      placeholder: __('Title'),
+                    },
+                    {
+                      lang: 'fr_CA',
+                      value: this.state.profile.titleFr,
+                      placeholder: __('Title'),
+                    },
+                  ]}
+                />
+              </Item.Meta>
+              <Item.Meta>
+                <ReactI18nEdit
+                  edit={this.state.editMode}
+                  lang={localizer.lang}
+                  values={[
+                    {
+                      lang: 'en_CA',
+                      value: this.state.profile.org.nameEn,
+                      placeholder: __('Organization'),
+                    },
+                    {
+                      lang: 'fr_CA',
+                      value: this.state.profile.org.nameFr,
+                      placeholder: __('Organization'),
+                    },
+                  ]}
+                />
+              </Item.Meta>
+              <Item.Description style={{ marginTop: '20px' }}>
+                <List horizontal>
+                  <List.Item>
+                    <List.Icon size="large" name="phone" />
+                    <List.Content>
+                      <List.Header> {__('Work')} </List.Header>
+                      <List.Description>
+                        <ReactI18nEdit
+                          edit={this.state.editMode}
+                          values={[{
+                            value: this.state.profile.officePhone,
+                            placeholder: __('Phone number'),
+                          }]}
+                          showLabel={false}
+                        />
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon size="large" name="mobile" />
+                    <List.Content>
+                      <List.Header> {__('Mobile')} </List.Header>
+                      <List.Description>
+                        <ReactI18nEdit
+                          edit={this.state.editMode}
+                          values={[{
+                            value: this.state.profile.mobilePhone,
+                            placeholder: __('Mobile phone number'),
+                          }]}
+                          showLabel={false}
+                        />
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon size="large" name="point" />
+                    <List.Content>
+                      <List.Header> {__('Address')} </List.Header>
+                      <List.Description>
+                        <ReactI18nEdit
+                          edit={this.state.editMode}
+                          values={[{
+                            value: this.state.profile.address.streetAddress,
+                            placeholder: __('Address'),
+                          }]}
+                          showLabel={false}
+                        />
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon size="large" name="mail" />
+                    <List.Content>
+                      <List.Header>{__('Email')} </List.Header>
+                      <List.Description>
+                        <ReactI18nEdit
+                          edit={this.state.editMode}
+                          values={[{
+                            value: this.state.profile.email,
+                            placeholder: __('Email'),
+                          }]}
+                          showLabel={false}
+                        />
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                </List>
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+    );
+  }
+}
 
 ProfileInfo.defaultProps = {
   profile: { org: {}, address: {} },
