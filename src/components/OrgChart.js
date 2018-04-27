6fcd4570e355d599ea3c2b0b5090104a952b049f
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2018-04-27 10:24:14 -0300

    added overflow on the org chart

--- src/components/OrgChart.js
+++ src/components/OrgChart.js
@@ -4,83 +4,123 @@ import PropTypes from 'prop-types';
 
 import GCOrgChart from '@gctools-components/react-gc-orgchart';
 
-const OrgChart = (props, context) => {
-  const {
-    loading,
-    error,
-    sup: { supervisor },
-    sup: me,
-    profiles,
-    gcID,
-    employees,
-  } = props;
-  if (error) return `Error!: ${error}`;
-  if (!me.gcID) return null;
-  const orgStructure = { subordinates: [] };
-  const orgTierName = (localizer.lang === 'en_CA') ? 'nameEn' : 'nameFr';
-  let subject = false;
-  if (supervisor && supervisor.gcID) {
-    orgStructure.name = supervisor.name;
-    orgStructure.uuid = supervisor.gcID;
-    orgStructure.orgTier = (supervisor.org) ? supervisor.org[orgTierName] : '';
-  } else {
-    orgStructure.name = me.name;
-    orgStructure.uuid = me.gcID;
-    orgStructure.orgTier = (me.org) ? me.org[orgTierName] : '';
-    subject = orgStructure;
+class OrgChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.element = false;
+    this.elementSafety = 0;
+    this.scrollSubjectIntoView = this.scrollSubjectIntoView.bind(this);
   }
-  if (profiles) {
-    profiles.forEach((p) => {
-      const i = orgStructure.subordinates.push({
-        name: p.name,
-        uuid: p.gcID,
-        orgTier: (p.org) ? p.org[orgTierName] : '',
+
+  componentDidMount() {
+    this.scrollSubjectIntoView();
+  }
+
+  componentWillReceiveProps(next) {
+    if (next.gcID !== this.props.gcID) {
+      this.scrollSubjectIntoView();
+    }
+  }
+
+  scrollSubjectIntoView() {
+    setTimeout(() => {
+      if (this.element) {
+        this.elementSafety = 0;
+        const focused = this.element.element.getElementsByClassName('focused');
+        if (focused.length === 1) {
+          focused[0].scrollIntoView({ inline: 'center' });
+        }
+      } else {
+        this.elementSafety += 1;
+        if (this.elementSafety < 100000) {
+          this.scrollSubjectIntoView();
+        } else {
+          throw Error('Element referencing failed.');
+        }
+      }
+    }, 100);
+  }
+
+  render() {
+    const {
+      loading,
+      error,
+      sup: { supervisor },
+      sup: me,
+      profiles,
+      gcID,
+      employees,
+    } = this.props;
+    if (error) return `Error!: ${error}`;
+    if (!me.gcID) return null;
+    const orgStructure = { subordinates: [] };
+    const orgTierName = (localizer.lang === 'en_CA') ? 'nameEn' : 'nameFr';
+    let subject = false;
+    if (supervisor && supervisor.gcID) {
+      orgStructure.name = supervisor.name;
+      orgStructure.uuid = supervisor.gcID;
+      orgStructure.orgTier = (supervisor.org) ?
+        supervisor.org[orgTierName] : '';
+    } else {
+      orgStructure.name = me.name;
+      orgStructure.uuid = me.gcID;
+      orgStructure.orgTier = (me.org) ? me.org[orgTierName] : '';
+      subject = orgStructure;
+    }
+    if (profiles) {
+      profiles.forEach((p) => {
+        const i = orgStructure.subordinates.push({
+          name: p.name,
+          uuid: p.gcID,
+          orgTier: (p.org) ? p.org[orgTierName] : '',
+        });
+        if (p.gcID === gcID) subject = orgStructure.subordinates[i - 1];
       });
-      if (p.gcID === gcID) subject = orgStructure.subordinates[i - 1];
-    });
-  }
-
-  if (subject && employees && employees.length > 0) {
-    subject.subordinates = [];
-    employees.forEach(p =>
-      subject.subordinates.push({
-        name: p.name,
-        uuid: p.gcID,
-        orgTier: (p.org) ? p.org[orgTierName] : '',
-      }));
-  }
-
-  const navigateToProfile = (uuid) => {
-    if (gcID !== uuid) {
-      context.router.history.push(`/profile/${uuid}`);
     }
-  };
+
+    if (subject && employees && employees.length > 0) {
+      subject.subordinates = [];
+      employees.forEach(p =>
+        subject.subordinates.push({
+          name: p.name,
+          uuid: p.gcID,
+          orgTier: (p.org) ? p.org[orgTierName] : '',
+        }));
+    }
+
+    const navigateToProfile = (uuid) => {
+      if (gcID !== uuid) {
+        this.context.router.history.push(`/profile/${uuid}`);
+      }
+    };
 
 
-  const onClick = (e, obj) => {
-    navigateToProfile(obj.uuid);
-  };
-
-  const onKeyPress = (e, obj) => {
-    if (e.key === 'Enter') {
+    const onClick = (e, obj) => {
       navigateToProfile(obj.uuid);
-    }
-  };
+    };
 
-  return (
-    <Segment>
-      <Dimmer active={loading}>
-        <Loader content="Loading" />
-      </Dimmer>
-      <GCOrgChart
-        orgStructure={orgStructure}
-        subject={gcID}
-        onClick={onClick}
-        onKeyPress={onKeyPress}
-      />
-    </Segment>
-  );
-};
+    const onKeyPress = (e, obj) => {
+      if (e.key === 'Enter') {
+        navigateToProfile(obj.uuid);
+      }
+    };
+
+    return (
+      <Segment style={{ overflowX: 'scroll' }}>
+        <Dimmer active={loading}>
+          <Loader content="Loading" />
+        </Dimmer>
+        <GCOrgChart
+          ref={(r) => { this.element = r; }}
+          orgStructure={orgStructure}
+          subject={gcID}
+          onClick={onClick}
+          onKeyPress={onKeyPress}
+        />
+      </Segment>
+    );
+  }
+}
 
 OrgChart.contextTypes = {
   router: PropTypes.object,
